462a946cefe86ba4b49c4901e38ddfac5d977c7e
Vercel <vercel[bot]@users.noreply.github.com>
2026-01-21 03:44:29 +0000

    Add Vercel Web Analytics to Next.js
    This is an automated pull request generated by Vercel to help install [Web Analytics](https://vercel.com/docs/analytics) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- magic-portfolio/src/app/page.tsx
+++ magic-portfolio/src/app/page.tsx
@@ -1,9 +1,8 @@
-import { Meta } from "@once-ui-system/core";
-import { home, baseURL } from "@/resources";
 import HomeContent from "@/components/HomeContent";
 import { Projects } from "@/components/work/Projects"; // Import Projects di sini (Server Side)
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/next"
+import { baseURL, home } from "@/resources";
+import { Meta } from "@once-ui-system/core";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export async function generateMetadata() {
   return Meta.generate({
@@ -22,7 +21,6 @@ export default function Home() {
       {/* Ini aman karena Projects dijalankan di Server, lalu hasilnya dikirim ke HomeContent */}
       <Projects />
       <SpeedInsights />
-      <Analytics />
     </HomeContent>
   );
 }
